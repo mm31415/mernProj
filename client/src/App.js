6315e0b97060c283d11cd79a97755189b324bbc3
mm31415 <martinez.m31415@gmail.com>
2018-11-18 12:02:04 -0500

    Add rendering of list of todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+// gql is used to parse the graphql string queries
+const TodosQuery = gql`
+  query {
+    todos {
+      id
+      text
+      complete
+    }
+  }
+`;
 
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        Hello
-      </div>
-    );
+    const { data: { loading, todos } } = this.props;
+    if (loading) {
+      return null;
+    } else {
+      return (
+        <div>
+          {todos.map((todo) => (
+            <div key={`${todo.id}-todo-item`}>{todo.text}</div>
+          ))}
+        </div>
+      );
+    }
   }
 }
 
-export default App;
+// this passes the component the todos based on the query
+export default graphql(TodosQuery)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// the client is the uri for the server
+const client = new ApolloClient({
+  uri: "http://localhost:4000"
+});
+
+// ApolloProvider is used to wrap the React App and letting it have access
+// to the server 
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
